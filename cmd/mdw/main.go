9a3ffe0daf5cf5a16fef47fc4619f9d0cad63709
jesse pike <jessepike@Mac.lan>
2026-01-22 20:47:39 -0600

    Initial commit: Project scaffolding

---
+++ cmd/mdw/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jessepike/markdown-workbench/pkg/engine"
+)
+
+func main() {
+	fmt.Println("Markdown Workbench Engine v0.1")
+	fmt.Println(engine.Status())
+}
---
+++ pkg/engine/engine.go
@@ -0,0 +1,5 @@
+package engine
+
+func Status() string {
+	return "Engine Online"
+}
